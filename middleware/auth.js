cb5d68dda8791be8c14f35160106b3e5fd42f981
Mayank Panwar <124695013+PANWAR-MAYANK@users.noreply.github.com>
2024-06-11 20:33:47 +0530

    Create auth.js

---
+++ middleware/auth.js
@@ -0,0 +1,15 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+module.exports = async (req, res, next) => {
+    const token = req.header('Authorization');
+    if (!token) return res.status(401).json({ message: 'Access denied' });
+
+    try {
+        const decoded = jwt.verify(token, 'your_jwt_secret');
+        req.user = decoded;
+        next();
+    } catch (error) {
+        res.status(400).json({ message: 'Invalid token' });
+    }
+};
